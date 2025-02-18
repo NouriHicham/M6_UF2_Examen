9c79a2089ba57e702773ed42412fdb4beaa66f00
NouriHicham <nourichamidhicham@fpllefia.com>
2025-02-18 18:40:35 +0100

    comecocos mueve y choca

--- src/vistas/Juegos.jsx
+++ src/vistas/Juegos.jsx
@@ -28,6 +28,72 @@ export default function Juegos(){
 
     }, []);
 
+    useEffect(() => {
+      function Teclas(event){
+         switch(event.key) {
+            case "ArrowRight":
+               moverDer();
+               break;
+            case "ArrowLeft":
+               moverIzq();
+               break;
+            case "ArrowDown":
+               bajar();
+               break;
+            case "ArrowUp":
+               subir();
+               break;
+            default:
+               break;
+         }
+      };
+
+      window.addEventListener("keydown", Teclas);
+      return () => window.removeEventListener("keydown", Teclas);
+      
+   }, [pacman]);
+
+   function moverDer(){
+      if (!Colision(pacman.x + 1, pacman.y)) {
+        actualizarPosicion(pacman.x, pacman.y, pacman.x + 1, pacman.y);
+      }
+    }
+    
+    function moverIzq(){
+      if (!Colision(pacman.x - 1, pacman.y)) {
+        actualizarPosicion(pacman.x, pacman.y, pacman.x - 1, pacman.y);
+      }
+    }
+    
+    function bajar(){
+      if (!Colision(pacman.x, pacman.y + 1)) {
+        actualizarPosicion(pacman.x, pacman.y, pacman.x, pacman.y + 1);
+      }
+    }
+    
+    function subir(){
+      if (!Colision(pacman.x, pacman.y - 1)) {
+        actualizarPosicion(pacman.x, pacman.y, pacman.x, pacman.y - 1);
+      }
+    }
+
+   function actualizarPosicion(antiguaX, antiguaY, x, y){
+      const nuevoCuadro = [...cuadro];
+      nuevoCuadro[antiguaY][antiguaX] = 0; // borrar antigua posicion
+      nuevoCuadro[y][x] = 2; // dibujar nueva posicion
+
+      setPacman({ ...pacman, x: x, y: y });
+      setCuadro(nuevoCuadro);
+   }
+
+   function Colision(x, y) {
+      if (x < 0 || x >= 20 || y < 0 || y >= 20) {
+        console.log('colision');
+        return true;
+      }
+      return false;
+    }
+
    return (
       <>
         <div className="points">Puntos: {points}</div>
